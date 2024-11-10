88c875d87f1046be0b6837167193a4735ba6bede
Bruce Quach <quachbruce@gmail.com>
2024-11-09 19:23:46 -0600

    attempt whitelist fix

--- server/cors-proxy-server.js
+++ server/cors-proxy-server.js
@@ -1,22 +1,20 @@
-import { createServer } from "cors-anywhere";
+import corsAnywhere from "cors-anywhere";
 
-// Set up CORS Anywhere's options
+// Set up CORS Anywhere options
 const corsOptions = {
-  // Whitelist specific origins (e.g., your React app's origin)
   originWhitelist: [
     "http://localhost:3002",
-    "https://code-bros.onrender.com/",
+    "https://code-bros.onrender.com",
     "https://palate-passport-backend.onrender.com",
   ],
+  requireHeader: ["origin", "x-requested-with"], // Require headers to prevent open proxy
+  removeHeaders: ["cookie", "cookie2"], // Remove cookies for security
 };
-
-// Create the CORS Anywhere server
-const server = createServer(corsOptions);
 
 // Define the port to run the proxy server on
 const PORT = process.env.PORT || 8080;
 
-// Start the server
-server.listen(PORT, () => {
+// Start the CORS Anywhere server
+corsAnywhere.createServer(corsOptions).listen(PORT, () => {
   console.log(`CORS Anywhere proxy is running on port ${PORT}`);
 });
